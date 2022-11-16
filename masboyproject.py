d578303a5a7479eaaef23f16ed989c75f2c3e0e8
micah masai kipkirui <103409704+Micah1999Masai@users.noreply.github.com>
2022-11-16 14:43:47 +0300

    Update masboyproject.py

--- masboyproject.py
+++ masboyproject.py
@@ -31,7 +31,7 @@ def main() :
 
 def predict_class(image) :
     with st.spinner('Loading Model...'):
-        classifier_model = keras.models.load_model(r'potatoes.h5', compile = False)
+        classifier_model = keras.models.load_model(r'masboy.h5', compile = False)
 
     shape = ((256,256,3))
     model = keras.Sequential([hub.KerasLayer(classifier_model, input_shape = shape)])    
