50e662fb5663d8eaa2632a55ab3fd7446dc861bd
micah masai kipkirui <103409704+Micah1999Masai@users.noreply.github.com>
2022-12-02 15:54:53 +0300

    Update masboyproject.py

--- masboyproject.py
+++ masboyproject.py
@@ -46,8 +46,8 @@ def predict_class(image) :
     prediction = model.predict(test_image)
     confidence = round(100 * (np.max(prediction[0])), 2)
     final_pred = class_name[np.argmax(prediction)]
-    recommendation = recom_names[np.argmax(prediction)]
-    return final_pred,recommendation,confidence
+    RECOMMENDATION = recom_names[np.argmax(prediction)]
+    return final_pred,RECOMMENDATION,confidence
 
 footer = """<style>
 a:link , a:visited{
